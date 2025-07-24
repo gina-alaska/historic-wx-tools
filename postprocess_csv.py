667e78f4bc1a06d5b235fd5d89f8fb1e4e8ba428
ojlarson <ojlarson@alaska.edu>
2025-07-24 08:23:19 -0800

    Improve functions

--- postprocess_csv.py
+++ postprocess_csv.py
@@ -4,18 +4,24 @@ import sys
 import os
 from pathlib import Path
 
-def create_combined_df(csv_dir: Path):
+def create_combined_df(csv_dir: Path) -> dd.DataFrame:
     
     df = dd.read_csv(csv_dir, delimiter=' ')
 
     return df
 
-def export_df(df, output_path):
-    df.to_csv(f'akurma_{year}.csv', index=False, single_file=True)
+def export_df(df: dd.DataFrame, output_path: Path) -> Path:
+    
+    df.to_csv(output_path, index=False, single_file=True)
+    
+    return output_path
 
-def zip_csv(csv_path):
-    with zipfile.ZipFile(csv_path + ".zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
+def zip_csv(csv_path: Path) -> Path:
+    
+    with zipfile.ZipFile(csv_path.with_suffix(".zip"), 'w', zipfile.ZIP_DEFLATED) as zipf:
         zipf.write(csv_path)
+    
+    return csv_path.with_suffix(".zip")
     
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -31,7 +37,7 @@ if __name__ == "__main__":
     df = create_combined_df(csv_dir)
     print(f"Exporting dataframe as {combined_csv_path}...")
     export_df(df, combined_csv_path)
-    print(f"Zipping {combined_csv_path} into {combined_csv_path + ".zip"}")
+    print(f"Zipping {combined_csv_path} into {combined_csv_path.with_suffix(".zip")}")
     zip_csv(combined_csv_path)
     
     
